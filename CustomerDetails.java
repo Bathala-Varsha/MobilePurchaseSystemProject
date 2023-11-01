9e2bf9422277c65e72d21da003b756fb49091de7
Bathala-Varsha <85429047+Bathala-Varsha@users.noreply.github.com>
2023-11-01 09:26:11 +0530

    Add files via upload

---
+++ CustomerDetails.java
@@ -0,0 +1,77 @@
+package anudip.JPAHibernate.entity;
+import java.util.Date;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+//annotation to create table
+@Entity
+//annotation to give table name
+@Table(name="Customerdetails")
+public class CustomerDetails
+{
+	//annotation for primary key
+		@Id
+		//annotation to generate automatic sequence
+		@GeneratedValue(generator="sequence-generator",strategy=GenerationType.SEQUENCE)
+		private int customerId;
+		//annotation to give column name
+		@Column(name="Customername")
+		private String cname;
+		private String mailId;
+		private String phoneno;
+		//gives current date and time
+		private Date purchasedate=new Date();
+		
+		@ManyToOne(cascade=CascadeType.ALL)
+		@JoinColumn(name="mobileId",referencedColumnName="mobileid")
+		private Mobiles Mobiles;
+		
+		//getters and setters
+		public int getCustomerId() {
+			return customerId;
+		}
+		public void setCustomerId(int customerId) {
+			this.customerId = customerId;
+		}
+		public String getCname() {
+			return cname;
+		}
+		public void setCname(String cname) {
+			this.cname = cname;
+		}
+		public String getMailId() {
+			return mailId;
+		}
+		public void setMailId(String mailId) {
+			this.mailId = mailId;
+		}
+		public String getPhoneno() {
+			return phoneno;
+		}
+		public void setPhoneno(String phoneno) {
+			this.phoneno = phoneno;
+		}
+		
+		
+		public Mobiles getMobiles() {
+			return Mobiles;
+		}
+		public void setMobiles(Mobiles mobiles) {
+			Mobiles = mobiles;
+		}
+		@Override
+		public String toString() {
+			return "Customerdetails [CustomereId=" + customerId + ", CustomerName=" + cname + ", MailId=" + mailId + ", Phoneno="
+					+ phoneno + ", Purchasedate=" + purchasedate + "]";
+		}
+		
+
+}
